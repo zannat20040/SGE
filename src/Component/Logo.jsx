168e4c014e444ab4e0bc9a7f91eb9020c632f97d
Zannatul Ferdousi Hema <shanzidadisha1234@gmail.com>
2024-06-21 19:11:46 +0600

    footer fix

--- src/Component/Logo.jsx
+++ src/Component/Logo.jsx
@@ -4,7 +4,7 @@ import logo from "../assets//Footer/SGE Logo V 2 1.png";
 const Logo = () => {
   return (
     <div className="flex flex-col gap-5 justify-center items-center border-b border-white pb-5">
-      <h1 className="text-2xl font-semibold text-white">Our Students are Our Reference</h1>
+      <h1 className="text-2xl font-semibold text-white text-center">Our Students are Our Reference</h1>
       <img src={logo} alt="" srcset="" />
       
     </div>
